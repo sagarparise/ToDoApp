32156a26385003573e3827996a5725f2d639a743
sagar parise <sagarparise01@gmail.com>
2023-12-16 18:31:14 +0530

    final

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 const searchBar = document.querySelector(".search-bar");
 const button = document.querySelector(".btn");
 const listContainer = document.getElementById("list-container");
+const number = document.querySelector(".right-number");
+const noTextBlock = document.querySelector(".no-item-text");
+let digit = parseInt(localStorage.getItem("number")) ?? 0;
 
 
 button.addEventListener("click", ()=>{
   if(searchBar.value !=='')
   {
+  
    let li = document.createElement("li");
    li.innerHTML = searchBar.value;
    listContainer.appendChild(li);
    let span = document.createElement("span");
    span.innerHTML = "\u00d7"
-   li.appendChild(span);
+   li.appendChild(span);  
+   textVisible();
+   digit++;
+   updateNumber(digit);
+   
   }else
   {
     alert("You must write something")
@@ -33,17 +41,48 @@ listContainer.addEventListener("click", function(e){
   {
     e.target.parentElement.remove();
     saveData();
+    digit--;
+    updateNumber(digit);
   }
 }, false);
+
+function textVisible(){
+  let listItems = listContainer.getElementsByTagName("li");
+  let length = listItems.length;
+  if(length)
+  {
+    noTextBlock.style.display = "none";
+  }
+  else{
+    noTextBlock.style.display = "block";
+  }
+}
+
+function updateNumber(digit) {
+  if(digit>=0)
+  {
+    number.innerHTML = digit;
+  }
+  
+}
 
 //save the data into the local storage 
 
 function saveData(){
-  localStorage.setItem("data", listContainer.innerHTML);
+localStorage.setItem("data", listContainer.innerHTML);
+localStorage.setItem("number",  number.innerHTML)
+  
 }
 
 
-function showTask(){
+function showTask() {
   listContainer.innerHTML = localStorage.getItem("data");
+  let listItems = listContainer.getElementsByTagName("li");
+  digit = listItems.length;
+  updateNumber(digit);
+  textVisible();
+ 
 }
+
 showTask();
+
